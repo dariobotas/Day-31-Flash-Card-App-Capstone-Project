f8d0062ed29a053a46074e4d24cf6ce8039878ed
dario <dario.botas@gmail.com>
2023-10-20 11:43:25 +0100

    pull and execution of the project from pycharm

--- part8/main_run.py
+++ part8/main_run.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter
 import pandas as pd
 import random as random
 
@@ -45,23 +45,23 @@ def main():
     canvas.itemconfig(word,text=current_card["English"], fill="white")
 
   
-  window = Tk()
+  window = tkinter.Tk()
   window.title("Flashy")
   window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
   
   flip_timer = window.after(3000, flip_card)
 
   #Images
-  wrong_img = PhotoImage(file="images/wrong.png")
-  right_img = PhotoImage(file="images/right.png")
-  card_back_img = PhotoImage(file="images/card_back.png")
-  card_front_img = PhotoImage(file="images/card_front.png")
+  wrong_img = tkinter.PhotoImage(file="images/wrong.png")
+  right_img = tkinter.PhotoImage(file="images/right.png")
+  card_back_img = tkinter.PhotoImage(file="images/card_back.png")
+  card_front_img = tkinter.PhotoImage(file="images/card_front.png")
 
   #Canvas
-  canvas = Canvas(width=800,
-                  height=526,
-                  bg=BACKGROUND_COLOR,
-                  highlightthickness=0)
+  canvas = tkinter.Canvas(width=800,
+                          height=526,
+                          bg=BACKGROUND_COLOR,
+                          highlightthickness=0)
   canvas_img = canvas.create_image(400, 263, image=card_front_img)
   canvas.grid(column=0, row=0, columnspan=2)
   language = canvas.create_text(400,
@@ -71,14 +71,14 @@ def main():
   word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
 
   #Buttons
-  wrong_button = Button(image=wrong_img,
-                        highlightbackground=BACKGROUND_COLOR,
-                        highlightthickness=0,
-                        command=next_card)
+  wrong_button = tkinter.Button(image=wrong_img,
+                                highlightbackground=BACKGROUND_COLOR,
+                                highlightthickness=0,
+                                command=next_card)
   wrong_button.grid(row=1, column=0)
-  right_button = Button(image=right_img,
-                        highlightthickness=0,
-                        command=card_learned)
+  right_button = tkinter.Button(image=right_img,
+                                highlightthickness=0,
+                                command=card_learned)
   right_button.grid(row=1, column=1)
 
   next_card()
